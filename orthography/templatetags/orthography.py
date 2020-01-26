993905542191fd6e1ef6fcadb2c6842b0875fe86
pavelsof <pavelsof@gmail.com>
2020-01-26 11:43:06 +0100

    The template filter date_ivanchov is born.

--- orthography/templatetags/orthography.py
+++ orthography/templatetags/orthography.py
@@ -1,7 +1,25 @@
+from datetime import date
+
 from django import template
 from django.template.defaultfilters import stringfilter
 
 from orthography import ivanchov
+
+
+MONTH_NAMES = (
+    'януарий',
+    'февруарий',
+    'мартъ',
+    'априлъ',
+    'май',
+    'юний',
+    'юлий',
+    'августъ',
+    'септемврий',
+    'октомврий',
+    'ноемврий',
+    'декемврий',
+)
 
 
 register = template.Library()
@@ -11,3 +29,13 @@ register = template.Library()
 @stringfilter
 def add_soft_hyphens(value):
     return ivanchov.add_soft_hyphens(value)
+
+
+@register.filter(expects_localtime=True)
+def date_ivanchov(value):
+    if value and isinstance(value, date):
+        return '{} {} {}'.format(
+            value.day, MONTH_NAMES[value.month-1], value.year
+        )
+    else:
+        return ''
