cfa7f0a9e49488917a30d381b647f67423af1584
Pavel Mihaylov <pavel@ontotext.com>
2020-01-30 20:09:45 +0200

    Added typography filter for articles: Converts -- to – (n-dash) and --- to — (m-dash). Converts ASCII double "quotes" to Bulgarian typographic „quotes“. Converts wrong typographic „quotes” or “quotes” to „quotes“.

---
+++ orthography/templatetags/typography.py
@@ -0,0 +1,13 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+from orthography import typography
+
+register = template.Library()
+
+#
+
+@register.filter
+@stringfilter
+def add_typography(value):
+    return typography.add_typography(value)
